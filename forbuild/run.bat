efb9bc217c7e60b57351f5269b41e6284e63e073
Dmitry Kuzmin <kuzmich.murom@gmail.com>
2015-10-17 00:40:00 +0300

    change run script

--- forbuild/run.bat
+++ forbuild/run.bat
@@ -1,1 +1,6 @@
-java -cp "lib\RXTXcomm.jar" -Djava.library.path=lib\x64 -jar DeviceDemoJavaFX.jar
+if defined ProgramFiles(x86) (
+	set arch=x64
+) else (
+	set arch=x86
+)
+	java -cp "lib\RXTXcomm.jar" -Djava.library.path=lib\%arch% -jar DeviceDemoJavaFX.jar
